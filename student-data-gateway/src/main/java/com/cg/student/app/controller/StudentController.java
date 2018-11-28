a20a316fc7afde22c5ae041cce538e208892b52f
CORP\RamaShanker <ramashanker@fico.com>
2018-11-28 17:29:45 +0530

    updated controller

--- student-data-gateway/src/main/java/com/cg/student/app/controller/StudentController.java
+++ student-data-gateway/src/main/java/com/cg/student/app/controller/StudentController.java
@@ -7,20 +7,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.client.RestTemplate;
 import com.cg.student.app.dto.Student;
+
 
 @RestController
 public class StudentController {
 	@RequestMapping(value = "/createData", method = RequestMethod.POST)
 	@ResponseBody
 	public Student createStudentData(@RequestBody Student student) {
-		return student;
+		final String uri = "http://localhost:8083/mongo/student/create"; 
+	    RestTemplate restTemplate = new RestTemplate();
+	    Student result = restTemplate.postForObject( uri, student, Student.class);
+	    System.out.println(result);
+	    return result;
 	}
 	
 	@RequestMapping(value = "/readData", method = RequestMethod.GET)
 	@ResponseBody
 	public List<Student> readStudentData() {
-		return null;
+		final String uri = "http://localhost:8083/mongo/student/read"; 
+	    RestTemplate restTemplate = new RestTemplate();
+	    List<Student> result = restTemplate.getForObject(uri, List.class);
+	    System.out.println(result.size());
+	    return result;
 	}
 }
--- student-data-gateway/src/main/java/com/cg/student/app/dto/Student.java
+++ student-data-gateway/src/main/java/com/cg/student/app/dto/Student.java
@@ -1,12 +1,10 @@
 package com.cg.student.app.dto;
+
 public class Student {
 
-	
 	private String fullName;
 	private String rollno;
 	private String std;
-
-	
 
 	public String getFullName() {
 		return fullName;
@@ -15,9 +13,26 @@ public class Student {
 	public void setFullName(String fullName) {
 		this.fullName = fullName;
 	}
+	
+	public String getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(String rollno) {
+		this.rollno = rollno;
+	}
+	
+	public String getStd() {
+		return rollno;
+	}
+
+	public void setStd(String std) {
+		this.std = std;
+	}
+
 
 	@Override
 	public String toString() {
-		return  ", fullName: " + this.fullName + ", Rollno: " + this.rollno + ", Standard: " + this.std;
+		return ", fullName: " + this.fullName + ", Rollno: " + this.rollno + ", Standard: " + this.std;
 	}
 }
