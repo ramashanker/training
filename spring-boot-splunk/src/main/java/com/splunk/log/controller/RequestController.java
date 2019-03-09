5b931aaafa57177ee8fd3ac904e520e77122de2b
ramashanker <ramashanker87@gmail.com>
2019-03-09 20:24:58 +0530

    updated message

--- spring-boot-splunk/src/main/java/com/splunk/log/controller/RequestController.java
+++ spring-boot-splunk/src/main/java/com/splunk/log/controller/RequestController.java
@@ -2,9 +2,7 @@ package com.splunk.log.controller;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,9 +11,9 @@ public class RequestController {
 
     private static final Log LOGGER = LogFactory.getLog(RequestController.class);
 
-    @GetMapping
-    public String hello(@RequestParam(name = "name", defaultValue = "World") String name) {
-        LOGGER.info("name="+name);
-        return "Hello, " + name;
+    @RequestMapping("/")
+    public String index() {
+    	LOGGER.info("Log sent to splunk!!");
+        return "Log sent to splunk!!";
     }
 }
